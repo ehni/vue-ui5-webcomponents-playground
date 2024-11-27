7cf3868e7c0435e9e6d6d100cfc1648787475a8e
Robert Ehni <ehni@users.noreply.github.com>
2024-11-27 10:34:25 +0100

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,132 @@
 <template>
-	<div>
-		<h1>Select problem</h1>
-		<p>Problem: Select does not show selected item. "All" (first item) should be selected after page load with the
-			":selected" property, instead the third/last item ("Completed") is selected.</p>
-		<ui5-select @change="updateStateFilter($event)">
-			<ui5-option v-for="stateFilter in stateFilters" :key="stateFilter.id" :data-value="stateFilter.id"
-				:selected="currentStateFilter.toLowerCase() === stateFilter.id.toLowerCase()">
-				{{ stateFilter.displayName }} (selected: {{ currentStateFilter.toLowerCase() ===
-					stateFilter.id.toLowerCase() }})
-			</ui5-option>
-		</ui5-select>
-		<p>Current state filter: {{ currentStateFilter }}</p>
-		<p>Possible filters:</p>
-		<ul>
-			<li v-for="filter in stateFilters" :key="filter.id">
-				id: {{ filter.id }} - displayName: {{ filter.displayName }}
-			</li>
-		</ul>
-	</div>
+  <ui5-shellbar primary-title="Dashboard">
+    <ui5-button icon="menu" slot="startButton" id="toggle"></ui5-button>
+    <ui5-avatar ref="profileButton" slot="profile">
+      <img style="max-height: 20px" :src="defaultAvatar" />
+    </ui5-avatar>
+    <ui5-button
+      slot="assistant"
+      ref="button1"
+      icon="feedback"
+      id="surveyButton"
+    ></ui5-button>
+    <ui5-shellbar-item
+      ref="button2"
+      icon="dark-mode"
+      @click="
+        (event) => {
+          if (popover2.open) {
+            popover2.open = false;
+          } else {
+            console.log('event.detail.targetRef is: ', event.detail.targetRef);
+            console.log('button2.value is: ', button2);
+            popover2.opener = event.detail.targetRef;
+            popover2.open = true;
+          }
+        }
+      "
+      text="Enable light mode"
+      title="Enable light mode"
+    ></ui5-shellbar-item>
+  </ui5-shellbar>
+  <ui5-popover ref="popover1" placement="Bottom">
+    <div class="popover-content">Tooltip #1 you will see me automatically.</div>
+  </ui5-popover>
+  <ui5-popover ref="popover2" placement="Bottom">
+    <div class="popover-content">
+      <ui5-text>
+        Tooltip #2, you only see me when manually clicked :/
+      </ui5-text>
+    </div>
+  </ui5-popover>
+  <p class="content">
+    This is a demo page. Upon loading the page, a tooltip should appear on the
+    feedback button as well as the theme button.
+    <br />
+    However, the second tooltip does not appear.
+    <br />
+    Problem: the ui5-shellbar-item is not the dom element rendered in the
+    shellbar, instead a ui5-button is generated under the hood and it is not
+    possible to get this button programmatically out of the shadow dom and use
+    it as an opener for the popover.
+    <br />
+    Trigger popovers manually:
+    <br />
+    <ui5-button @click="openPopover1">Open Popover 1</ui5-button>
+    <br />
+    <ui5-button @click="openPopover2">Open Popover 2</ui5-button>
+    <br />
+    Opening the popover2 does not work programmatically, however clicking on the
+    theme button in the toolbar will open the tooltip and you can see the
+    differene in the ui5-shellbar-item value and the actual rendered dom element
+    which can be used as an opener.
+  </p>
 </template>
 
-<script>
-import '@ui5/webcomponents/dist/Select.js';
-import '@ui5/webcomponents/dist/Option.js';
+<style lang="css">
+html,
+body {
+  height: 100%;
+  margin: 0;
+}
 
-export default {
-	components: {
-	},
-	data() {
-		return {
-			currentStateFilter: 'all',
-			stateFilters: [
-				{ id: 'all', displayName: 'All' },
-				{ id: 'active', displayName: 'Active' },
-				{ id: 'completed', displayName: 'Completed' }
-			],
-		}
-	},
-	methods: {
-		updateStateFilter(event) {
-			const targetValue = event.detail.selectedOption.dataset.value
-			console.log('updating state: ', targetValue);
-			this.currentStateFilter =
-				this.stateFilters.find(filter => filter.displayName.toLowerCase() === targetValue.toLowerCase())?.id || 'all';
-		},
-		onReturnPressed() {
-			console.log('return pressed');
-			alert('return pressed');
-		}
+body {
+  background-color: lightgrey !important;
+}
 
-	}
-};
+.content {
+  padding: 2rem;
+}
+
+.popover-content {
+  display: flex;
+  flex-direction: column;
+  padding: 0.5rem;
+}
+</style>
+
+<script setup>
+import defaultAvatar from "./assets/images/default_avatar.jpg";
+import { ref, onMounted } from "vue";
+
+import "@ui5/webcomponents-icons/dist/AllIcons.js";
+
+import "@ui5/webcomponents-fiori/dist/ShellBar.js";
+import "@ui5/webcomponents-fiori/dist/ShellBarItem.js";
+
+import "@ui5/webcomponents/dist/Avatar.js";
+import "@ui5/webcomponents/dist/Bar.js";
+import "@ui5/webcomponents/dist/CheckBox.js";
+import "@ui5/webcomponents/dist/Button.js";
+import "@ui5/webcomponents/dist/Popover.js";
+
+const popover1 = ref(null);
+const popover2 = ref(null);
+
+const button1 = ref(null);
+const button2 = ref(null);
+
+onMounted(() => {
+  console.log("Opening both popvers in 2s");
+  setTimeout(() => {
+    openPopover1();
+    openPopover2();
+  }, 2000);
+});
+
+function openPopover1() {
+  if (popover1.value) {
+    console.log("button1.value is: ", button1.value);
+    popover1.value.opener = button1.value;
+    popover1.value.open = true;
+  }
+}
+
+function openPopover2() {
+  if (popover2.value) {
+    console.log("button2.value is: ", button2.value);
+    popover2.value.opener = button2.value;
+    popover2.value.open = true;
+  }
+}
 </script>
-
-<style></style>
